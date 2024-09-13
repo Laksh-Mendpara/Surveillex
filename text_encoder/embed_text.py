7508c190e61d2afac94b6360265565ee0ce35008
pack0shades <kumarpragay@gmail.com>
2024-09-14 00:06:40 +0530

    updated embed_text.py

--- text_encoder/embed_text.py
+++ text_encoder/embed_text.py
@@ -7,18 +7,20 @@ from transformers import BertTokenizer, BertModel
 '''model = models.video.r3d_18(pretrained=True)
 
 model.eval()'''
-definitions = {'Abuse': 'cruel or inhumane treatment', 'Arrest': 'the act of apprehending (especially apprehending a criminal)', 'Arson': 'malicious burning to destroy property', 'Assault': 'close fighting during the culmination of a military attack', 'Burglary': 'entering a building unlawfully with intent to commit a felony or to steal valuable property', 'Explosion': 'a violent release of energy caused by a chemical or nuclear reaction', 'Fighting': 'the act of fighting; any contest or struggle', 'Normal Videos': 'Regular video content without criminal activity.', 'RoadAccidents': 'Accidents occurring on the road involving vehicles.', 'Robbery': 'larceny by threat of violence', 'Shooting': 'the act of firing a projectile', 'Shoplifting': 'the act of stealing goods that are on display in a store', 'Stealing': 'the act of taking something from someone unlawfully', 'Vandalism': 'willful wanton and malicious destruction of the property of others'}
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-model = BertModel.from_pretrained('bert-base-uncased')
-embeddings = []
-for definition in definitions.values():
-    inputs = tokenizer(definition, return_tensors='pt')
-    outputs = model(**inputs)
-    text_embedding = outputs.last_hidden_state
-    embeddings.append(text_embedding)
+def get_text_embeddings(definitions):
+    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    model = BertModel.from_pretrained('bert-base-uncased')
+    embeddings = []
+    for definition in definitions.values():
+        inputs = tokenizer(definition, return_tensors='pt')
+        outputs = model(**inputs)
+        text_embedding = outputs.last_hidden_state
+        embeddings.append(text_embedding)
+    print(f"shape of embeddings:{len(embeddings)}")
+    return embeddings
 
 
-print(text_embedding.shape)
+
 
 
 '''class MultiModalTransformer(nn.Module):
@@ -41,3 +43,8 @@ fused_embedding = multi_modal_transformer(video_embedding, text_embedding)
 print("Fused Embedding Shape:", fused_embedding.shape)
 '''
 
+if __name__ == '__main__':
+    definitions = {'Abuse': 'cruel or inhumane treatment', 'Arrest': 'the act of apprehending (especially apprehending a criminal)', 'Arson': 'malicious burning to destroy property', 'Assault': 'close fighting during the culmination of a military attack', 'Burglary': 'entering a building unlawfully with intent to commit a felony or to steal valuable property', 'Explosion': 'a violent release of energy caused by a chemical or nuclear reaction', 'Fighting': 'the act of fighting; any contest or struggle', 'Normal Videos': 'Regular video content without criminal activity.', 'RoadAccidents': 'Accidents occurring on the road involving vehicles.', 'Robbery': 'larceny by threat of violence', 'Shooting': 'the act of firing a projectile', 'Shoplifting': 'the act of stealing goods that are on display in a store', 'Stealing': 'the act of taking something from someone unlawfully', 'Vandalism': 'willful wanton and malicious destruction of the property of others'}
+    embeddings = get_text_embeddings(definitions)
+    print (definitions)
+    print(embeddings)
