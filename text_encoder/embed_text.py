2b0e89d1dbfa50390c6f692c70adc49399c55816
pack0shades <kumarpragay@gmail.com>
2024-09-14 00:39:12 +0530

    added function to obtain SBERT embeddings

--- text_encoder/embed_text.py
+++ text_encoder/embed_text.py
@@ -3,11 +3,12 @@ import torchvision.models as models
 import nltk
 from nltk.corpus import wordnet as wn
 from transformers import BertTokenizer, BertModel
+from sentence_transformers import SentenceTransformer
 
 '''model = models.video.r3d_18(pretrained=True)
 
 model.eval()'''
-def get_text_embeddings(definitions):
+def get_text_embeddings_BERT(definitions):
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
     model = BertModel.from_pretrained('bert-base-uncased')
     embeddings = []
@@ -19,7 +20,15 @@ def get_text_embeddings(definitions):
     print(f"shape of embeddings:{len(embeddings)}")
     return embeddings
 
-
+def get_text_embeddings_SBERT(definitions):
+    model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+    embeddings = []
+    for definition in definitions.values():
+        text_embedding = model.encode(definition, convert_to_tensor=True)
+        embeddings.append(text_embedding)
+    
+    print(f"Shape of embeddings: {len(embeddings)}")
+    return embeddings
 
 
 
@@ -45,6 +54,8 @@ print("Fused Embedding Shape:", fused_embedding.shape)
 
 if __name__ == '__main__':
     definitions = {'Abuse': 'cruel or inhumane treatment', 'Arrest': 'the act of apprehending (especially apprehending a criminal)', 'Arson': 'malicious burning to destroy property', 'Assault': 'close fighting during the culmination of a military attack', 'Burglary': 'entering a building unlawfully with intent to commit a felony or to steal valuable property', 'Explosion': 'a violent release of energy caused by a chemical or nuclear reaction', 'Fighting': 'the act of fighting; any contest or struggle', 'Normal Videos': 'Regular video content without criminal activity.', 'RoadAccidents': 'Accidents occurring on the road involving vehicles.', 'Robbery': 'larceny by threat of violence', 'Shooting': 'the act of firing a projectile', 'Shoplifting': 'the act of stealing goods that are on display in a store', 'Stealing': 'the act of taking something from someone unlawfully', 'Vandalism': 'willful wanton and malicious destruction of the property of others'}
-    embeddings = get_text_embeddings(definitions)
+    BERT_embeddings = get_text_embeddings_BERT(definitions)
+    SBERT_embeddings = get_text_embeddings_SBERT(definitions)
     print (definitions)
-    print(embeddings)
+    '''print(BERT_embeddings)'''
+    print(SBERT_embeddings)
