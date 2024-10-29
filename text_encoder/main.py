f317ef6d7c8c6971a3c2387229cbe7fb349124cb
Laksh-Mendpara <b23cs1037@iitj.ac.in>
2024-10-29 19:43:55 +0530

    saving in pkl format

--- text_encoder/main.py
+++ text_encoder/main.py
@@ -1,6 +1,7 @@
 from wordnet import get_definition
 from description_generation import generate_expanded_definition
 from embed_text import get_text_embeddings_BERT, get_text_embeddings_SBERT
+import pickle
 
 def generate_final_text_embedding_BERT(file_path):
     word_def = get_definition(file_path)
@@ -16,12 +17,16 @@ def generate_final_text_embedding_SBERT(file_path):
     print(exp_def)
     print(type(exp_def))
     bert_embeddings = get_text_embeddings_SBERT(exp_def)
+
+    embeddings_with_labels = {label: embedding for label, embedding in zip(exp_def.keys(), bert_embeddings)}
+    with open('./data/embeddings_with_labels.pkl', 'wb') as f:
+        pickle.dump(embeddings_with_labels, f)
+
+    print("Embeddings and labels saved successfully.")
     return bert_embeddings
+
 
 if __name__ == '__main__':
     file = './data/ucf_crime_labels.json'
-    BERT_embeddings = generate_final_text_embedding_BERT(file)
     SBERT_embeddings = generate_final_text_embedding_SBERT(file)
-    print(BERT_embeddings)
     print(SBERT_embeddings)
-    
